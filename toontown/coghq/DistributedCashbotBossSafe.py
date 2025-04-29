2fd8f4558ee3b16715f7537fd5a83f8171baddbd
Lou <thetycoon90@gmail.com>
2025-04-29 16:27:47 +0400

    remove more speed caching references

--- toontown/coghq/DistributedCashbotBossSafe.py
+++ toontown/coghq/DistributedCashbotBossSafe.py
@@ -129,7 +129,6 @@ class DistributedCashbotBossSafe(DistributedCashbotBossObject.DistributedCashbot
         self.handler.addAgainPattern(self.collideName + '-%in')
         
         self.watchDriftName = self.uniqueName('watchDrift')
-        self.startCacheName = self.uniqueName('startSpeedCaching')
 
     def getMinImpact(self):
         # This method returns the minimum impact, in feet per second,
