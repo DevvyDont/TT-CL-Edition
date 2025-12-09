e715d19e34236d17a61f03022afb328397b7014d
Kitt Kitt Lou <thetycoon90@gmail.com>
2025-12-09 19:49:07 +0400

    back side + bigger goon

--- toontown/suit/DistributedCashbotBossAI.py
+++ toontown/suit/DistributedCashbotBossAI.py
@@ -629,7 +629,7 @@ class DistributedCashbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
         self.goonMovementTime = globalClock.getFrameTime()
         if side == None:
             if not self.wantOpeningModifications:
-                side = random.choice(['EmergeA', 'EmergeB'])
+                side = random.choice(['EmergeB', 'EmergeB'])
             else:
                 for t in self.involvedToons:
                     avId = t
@@ -670,12 +670,12 @@ class DistributedCashbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
             print("Elapsed Time: %s" % elapsed)
             if self.wantCraneThreePractice:
                 if elapsed > 5:
-                    goon_scale = 0.61
+                    goon_scale = 0.625
                 else:
                     goon_scale = self.progressRandomValue(self.goonMinScale, self.goonMaxScale, noRandom=self.wantMaxSizeGoons)
             else:
                 if elapsed > 5:
-                    goon_scale = 0.61
+                    goon_scale = 0.615
                 else:
                     goon_scale = self.progressRandomValue(self.goonMinScale, self.goonMaxScale, noRandom=self.wantMaxSizeGoons)
 
